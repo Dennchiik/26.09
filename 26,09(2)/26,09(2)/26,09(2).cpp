fce1bb326e8cdf18c834d83be31f66433af4e1fd
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-10-05 22:47:29 +0300

    Add files via upload

---
+++ 26,09(2)/26,09(2)/26,09(2).cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+
+const int arraySize = 20;
+const int minValue = 1;
+const int maxValue = 20;
+
+int getRandomNumber(int min, int max) {
+    return min + rand() % (max - min + 1);
+}
+
+void shuffleArray(int arr[], int size) {
+    for (int i = size - 1; i > 0; --i) {
+        int j = getRandomNumber(0, i);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+int findPosition(int arr[], int size, int value) {
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void customSort(int arr[], int left, int right) {
+    for (int i = left; i <= right; ++i) {
+        for (int j = i + 1; j <= right; ++j) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[arraySize];
+
+    for (int i = 0; i < arraySize; ++i) {
+        arr[i] = minValue + i;
+    }
+
+    srand(time(NULL));
+    shuffleArray(arr, arraySize);
+
+    int randomValue = getRandomNumber(minValue, maxValue);
+    int position = findPosition(arr, arraySize, randomValue);
+
+    if (position != -1) {
+        customSort(arr, 0, position - 1);
+        customSort(arr, position + 1, arraySize - 1);
+    }
+
+    for (int i = 0; i < arraySize; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ 26,09/26,09/26,09.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+int findLeftNegIndex(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] < 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int findRightNegIndex(int arr[], int size) {
+    for (int i = size - 1; i >= 0; --i) {
+        if (arr[i] < 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void sortBetweenIndices(int arr[], int leftIndex, int rightIndex) {
+    for (int i = leftIndex + 1; i < rightIndex; ++i) {
+        for (int j = i + 1; j < rightIndex; ++j) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    const int arraySize = 10;
+    int arr[arraySize] = { -10, 5, 7, -15, 8, -3, 12, -17, 4, -9 };
+
+    int leftNegIndex = findLeftNegIndex(arr, arraySize);
+    int rightNegIndex = findRightNegIndex(arr, arraySize);
+
+    if (leftNegIndex != -1 && rightNegIndex != -1) {
+        sortBetweenIndices(arr, leftNegIndex, rightNegIndex);
+
+        for (int i = 0; i < arraySize; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    else {
+        std::cout << "В массиве нет отрицательных элементов." << std::endl;
+    }
+
+    return 0;
+}
